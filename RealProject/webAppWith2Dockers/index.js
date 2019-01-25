752dade28481601d42e3b20ab82b9ac1587b87bc
GhaidaAt95 <ghaida.alatoum2@gmail.com>
2019-01-25 15:48:50 -0600

    Docker Compose

--- RealProject/webAppWith2Dockers/index.js
+++ RealProject/webAppWith2Dockers/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const redis = require('redis');
 
+const process = require('process');
+
 const app = express();
 
 const client = redis.createClient({
@@ -14,9 +16,11 @@ const client = redis.createClient({
 client.set('visits',0);
 
 app.get('/',(req, res)=> {
+    // Crash it to practice restarting
+    process.exit(0);
     client.get('visits',(err, visits)=>{
         res.send('Number of visits is '+ visits);
-        client.set('visits', paresInt(visits + 1));
+        client.set('visits', parseInt(visits)+1);
     });
 });
 
